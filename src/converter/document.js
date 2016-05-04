463d010b4c43b9218561c267c92c09f9c271a9da
licheng <lalalic@139.com>
2016-05-05 07:14:58 +0800

    named style structure

--- src/converter/document.js
+++ src/converter/document.js
@@ -12,16 +12,52 @@ export default class Document extends Converter{
         this.doc=this.constructor.createDocument()
         this.root=this.doc.root=this.content=this.doc.documentElement
         this.layoutMasterSet=this.doc.layoutMasterSet=this.content.firstChild
-		
+
+        let styles=[] ,wrappers=new Map()
+        wrappers.set('*',this.root)
+
 		Object.assign(this.doc,{
 			createStyle(id, pid){
-				return Object.seal(new Style(id, pid))
+                let wrapper=this.createElement("wrapper")
+                let parent=wrappers.get(pid)
+                if(parent)
+                    parent.appendChild(wrapper)
+                else
+                    styles.push(wrapper)
+
+                wrappers.set(id,wrapper)
+                return wrapper
 			},
-			
-			getStyle(id){
-				return Style.tree.get(id)
-			}
+
+            applyStyleOn(el,styleId){
+                let style=wrappers.get(styleId)
+                let outerWrapper, innerWrapper
+                if(style){
+                    innerWrapper=style.clone()
+                    let current=innerWrapper
+                    while(style.parentNode){
+                        outerWrapper=style.parentNode.clone()
+                        outerWrapper.appendChild(current)
+                        current=outerWrapper
+                        style=style.parentNode
+                    }
+                }else{
+
+                }
+                el.parentNode.appendChild(outerWrapper)
+                innerWrapper.appendChild(el)
+            }
+
+			__release(){
+                wrappers.clear()
+                delete styles
+                delete wrappers
+            }
 		})
+    }
+
+    release(){
+        this.doc.__release()
     }
 }
 
@@ -31,27 +67,27 @@ class Style{
 		this.categories=new Map()
 		this.id=id
 		this.pid=pid
-		
+
 		id && Style.tree.set(id, this)
 	}
-	
+
 	setAttribute(){// a unified API for element
 		this.set(...arguments)
 	}
-	
+
 	set(){
 		this.props.set(...arguments)
 	}
-	
+
 	addCategory(category,style){
 		this.categories.set(category,style)
 		return style
 	}
-	
+
 	get parent(){
 		return Style.tree.get(this.pid)
 	}
-	
+
 	applyOn(content){
 		console.log(`style ${this.id} applied`)
 		this.props.forEach((v,k)=>{
